2d7e30ec85bca6f16cbbdc5bab1bdea7e1519ee0
Jeffrey Leung <jleung513@gmail.com>
2019-01-26 15:57:53 -0800

    Reconfigure HTTP dispatcher to service requests

--- server/server.js
+++ server/server.js
@@ -1,7 +1,56 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var http = require('http');
+var request = require('request');
+var HttpDispatcher = require('httpdispatcher');
+var dispatcher = new HttpDispatcher();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const express = require('express');
+const app = express();
 
-app.listen(port, () => console.log(`[INFO] Server listening on port ${port}.`))
+const serverOptions = {
+  port: process.env.PORT || 3000
+};
+
+const mappings = {
+  'home': '/',
+  'ask': '/ask'
+};
+
+// This function sets the URL mappings for endpoints.
+function setMappings(dispatcher) {
+
+  dispatcher.onGet(mappings['home'], function(req, response) {
+    response.writeHead(200);
+    console.log('Hello World!');
+    console.log();
+    response.end('Hello World!');
+  });
+
+  dispatcher.onPost(mappings['ask'], function(req, response) {
+    response.writeHead(201);
+    console.log('Replying to request with HTTP ' + response.statusCode);
+    console.log();
+    response.end('Hello World!');
+  });
+
+}
+
+function main() {
+  setMappings(dispatcher);
+
+  var server = http.createServer(function(request, response) {
+    console.log('Mapped ' + request.url);
+    console.log();
+    try {
+      dispatcher.dispatch(request, response);
+    } catch (except) {
+      console.log('Error: ' + except);
+    }
+  });
+
+  server.listen(serverOptions, function() {
+    console.log('[INFO] Server listening on http://localhost:%s.', serverOptions['port']);
+    console.log();
+  });
+}
+
+main()
