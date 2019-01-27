e584187bc8f92ccadf951431444e2131c49e32c4
Jeffrey Leung <jleung513@gmail.com>
2019-01-26 19:34:00 -0800

    Update states of a request

--- server/database.js
+++ server/database.js
@@ -10,8 +10,7 @@ function copyJsonObject(object) {
 
 const requestState = [
   'unmatched',
-  'matched-unconfirmed',
-  'matched-confirmed',
+  'matched-new',
   'matched-inprogress',
   'fulfilled'
 ];
